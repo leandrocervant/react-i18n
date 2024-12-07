9750b07a18fbbcc17c2bd196aeef66b00046cdfe
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2024-12-07 10:11:45 -0300

    Refactor HTML structure, remove unused CSS, and add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { t, useI18n } from "./i18n";
 
-import "./App.css";
-
-const classNames = {
-  root: "App",
-};
-
 function App() {
   const { languages, langCode, setLanguage } = useI18n();
 
   return (
-    <div className={classNames.root}>
+    <div>
       <p>{t("messages.hello")}</p>
       <select value={langCode} onChange={(e) => setLanguage(e.target.value)}>
         {languages.map(({ code, label }) => (
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import { I18nProvider } from "./i18n";
 import App from "./App";
 
+import "./index.css";
+
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
